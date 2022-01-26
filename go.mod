64c03c20c2938694d79a99ed9a3485864dab8136
bootun <bootunzhao@outlook.com>
2022-01-26 19:42:45 +0800

    feat:initialize token

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/bootun/tun
+
+go 1.18
---
+++ token/token.go
@@ -0,0 +1,35 @@
+package token
+
+type Type string
+
+type Token struct {
+	Literal string
+	Type    Type
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers + Literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	FUNCTION = "FUNCTION"
+	VAR      = "VAR"
+)
