7b84afc9621838f83f0d28a0fd8e54ea2003159b
bootun <bootunzhao@outlook.com>
2022-01-27 19:03:18 +0800

    fix:improve test case

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,1 +1,29 @@
 package lexer
+
+import "github.com/bootun/tun/token"
+
+type Lexer struct {
+	input        string
+	position     int // current position in input
+	readPosition int
+	ch           byte
+}
+
+func (l *Lexer) NextToken() token.Token {
+	tok := token.Token{
+		Literal: "",
+		Type:    "",
+	}
+	// TODO: implement me
+	return tok
+}
+
+func (l *Lexer) readChar() {
+	// TODO: implement me
+}
+
+func New(src string) *Lexer {
+	// TODO: fix position
+	l := &Lexer{input: src}
+	return l
+}
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,8 +1,34 @@
 package lexer
 
-import "testing"
+import (
+	"github.com/bootun/tun/token"
+	"testing"
+)
 
 func TestNextToken(t *testing.T) {
 
-	// TODO: improve test case
+	src := `+=(){}`
+
+	var tests = []struct {
+		expectedType    token.Type
+		expectedLiteral string
+	}{
+		{token.PLUS, "+"},
+		{token.ASSIGN, "="},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+	}
+
+	lexer := New(src)
+	for i, tt := range tests {
+		tok := lexer.NextToken()
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. excepted: %q, got: %q", i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. excepted: %q, got: %q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
 }
